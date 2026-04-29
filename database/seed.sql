b2f22a9607fae0724bd4921b7bf931c91d2e0c85
ballonaxx3 <jeremyballona25@gmail.com>
2026-04-29 00:05:22 -0600

    Add seed data

---
+++ database/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (email, password, balance, is_admin) VALUES
+('admin@codexx3.com', '$2y$10$e0NRF2H6y3G2m0h1V9Q9ZevRkV7vZ5g6o5Qe1sKq1q9yV3F1pG2yW', 100.00, 1);
+
+INSERT INTO services (name, category, rate, min_qty, max_qty) VALUES
+('Instagram Followers', 'Instagram', 1.20, 10, 10000),
+('YouTube Views', 'YouTube', 0.80, 100, 50000);
